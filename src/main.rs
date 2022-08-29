74d53657d740eb424917f8d431e289308e4610e5
Casey Rodarmor <casey@rodarmor.com>
2022-08-29 00:27:58 -0700

    Check links on CI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use {
+  chrono::naive::NaiveDate,
+  serde::Deserialize,
+  std::{collections::BTreeSet, fs},
+  url::Url,
+};
+
+#[derive(Deserialize)]
+struct Link {
+  title: String,
+  url: Url,
+  tags: BTreeSet<Tag>,
+  date: NaiveDate,
+  summary: String,
+}
+
+#[derive(Ord, Eq, PartialEq, PartialOrd, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+enum Tag {
+  Lightning,
+  Research,
+  Paper,
+  Security,
+}
+
 fn main() {
-    println!("Hello, world!");
+  let yaml = fs::read_to_string("links.yaml").unwrap();
+  serde_yaml::from_str::<Vec<Link>>(&yaml).unwrap();
 }
